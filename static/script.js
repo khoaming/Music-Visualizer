ac46f638766875efbfd75e4e6818d96736e8ce5e
jessemckenna <20506673+jessemckenna@users.noreply.github.com>
2018-06-22 15:58:18 -0700

    Add simple p5.js canvas

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
-// all javascript go here
+// Set up canvas
+function setup() {
+	createCanvas(windowWidth, windowHeight - 50);
+}
+
+// Draw content
+function draw() {
+	background(0); // black
+
+	fill(255,0,0);
+	ellipse(50, 50, 10); // circle(x, y, diameter)
+}
+
+// Resize canvas with window
+window.addEventListener('resize', function(event) {
+	resizeCanvas(windowWidth, windowHeight - 50);
+})
