c432dc6d5c95d76681d70f36a67331afd4bf1039
khoanguyen289 <nguyekho@oregonstate.edu>
2018-06-22 16:07:12 -0400

    add bootstrap and boiler plates

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, send_file, jsonify, redirect, url_for
+
+TEMPLATE_DIR = os.path.abspath('templates')
+STATIC_DIR = os.path.abspath('static')
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path='/static')
 app = Flask(__name__) # create instance of Flask
 
 # Main page route
 @app.route('/')
-def hello():
-	return "Hello!"
+def form():
+	return send_file('templates/index.html')
 
 if __name__ == '__main__':
 	app.run()
