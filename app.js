fbed3917ba020de4d88b714d914cf2088f35c330
jessemckenna <20506673+jessemckenna@users.noreply.github.com>
2018-06-23 21:26:22 -0700

    Implement weather search and weather icon

--- app.js
+++ app.js
@@ -9,30 +9,31 @@ app.use('/', express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const DEFAULT_WEATHER = "800"; // OpenWeatherMap sunny day code
+const DEFAULT_WEATHER = "800"; // OpenWeatherMap code for sunny and clear
+const DEFAULT_TIME = "day";
 
 // Homepage
 app.get('*', function (req, res) {
-	res.render("index", {weather: DEFAULT_WEATHER});
+	res.render("index", {weather: DEFAULT_WEATHER, time: DEFAULT_TIME});
 })
 
-// Homepage with custom zip
+// Homepage with weather by zip code
 app.post('/', function (req, res) {
 	var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + req.body.zip + ",us&APPID=" + config.getOWMKey();
 	request(url, function(err, response, body) {
 		if (err) {
 			console.log(err);
-			res.render("index", {weather: DEFAULT_WEATHER});
+			res.render("index", {weather: DEFAULT_WEATHER, time: DEFAULT_TIME});
 		}
 		else {
 			var weatherResponse = JSON.parse(body);
 			if (weatherResponse.main == undefined)
 			{
-				res.render("index", {weather: DEFAULT_WEATHER});
+				res.render("index", {weather: DEFAULT_WEATHER, time: DEFAULT_TIME});
 			}
 			else
 			{
-				res.render("index", {weather: weatherResponse.main.id});
+				res.render("index", {weather: weatherResponse.weather[0].id, time: DEFAULT_TIME});
 			}
 		}
 	})
