e42323f332064b5d78989ae02cf918863f855c85
jessemckenna <20506673+jessemckenna@users.noreply.github.com>
2018-06-23 18:26:40 -0700

    Change view engine to ejs for more flexibility later

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use('/', express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Homepage
 app.get('*', function (req, res) {
-	res.sendFile("index.html");
+	res.render("index");
 })
 
 const port = process.env.PORT || 5000;
