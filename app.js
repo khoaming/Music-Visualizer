91bc884cdb8a80ee8ce6c304b8f668a0c3bc303c
jessemckenna <20506673+jessemckenna@users.noreply.github.com>
2018-06-23 21:01:02 -0700

    Draft weather API functions

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
+
+const config = require('./config.js'); // API keys
 
 app.use('/', express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const DEFAULT_WEATHER = "800"; // OpenWeatherMap sunny day code
+
 // Homepage
 app.get('*', function (req, res) {
-	res.render("index");
+	res.render("index", {weather: DEFAULT_WEATHER});
+})
+
+// Homepage with custom zip
+app.post('/', function (req, res) {
+	var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + req.body.zip + ",us&APPID=" + config.getOWMKey();
+	request(url, function(err, response, body) {
+		if (err) {
+			console.log(err);
+			res.render("index", {weather: DEFAULT_WEATHER});
+		}
+		else {
+			var weatherResponse = JSON.parse(body);
+			if (weatherResponse.main == undefined)
+			{
+				res.render("index", {weather: DEFAULT_WEATHER});
+			}
+			else
+			{
+				res.render("index", {weather: weatherResponse.main.id});
+			}
+		}
+	})
 })
 
 const port = process.env.PORT || 5000;
