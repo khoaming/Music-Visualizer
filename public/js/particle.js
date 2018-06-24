3cd3b53e37113b7a04ae2ed016d62d2cfece2366
khoanguyen289 <nguyekho@oregonstate.edu>
2018-06-24 00:44:34 -0400

    random colors

--- public/js/particle.js
+++ public/js/particle.js
@@ -1,35 +1,39 @@
 function Particle(x, y) {
-  this.x = x;
-  this.y = y;
+    this.x = x;
+    this.y = y;
 
-  this.history = [];
+    this.history = [];
 
-  this.update = function() {
-    this.x += random(-10, 10);
-    this.y += random(-10, 10);
+    this.update = function() {
+        this.x += random(-10, 10);
+        this.y += random(-10, 10);
 
-    for (var i = 0; i < this.history.length; i++) {
-      this.history[i].x += random(-2, 2);
-      this.history[i].y += random(-2, 2);
+        for (var i = 0; i < this.history.length; i++) {
+            this.history[i].x += random(-2, 2);
+            this.history[i].y += random(-2, 2);
+        }
+
+        var v = createVector(this.x, this.y);
+        this.history.push(v);
+        if (this.history.length > 100) {
+            this.history.splice(0, 1);
+        }
     }
 
-    var v = createVector(this.x, this.y);
-    this.history.push(v);
-    if (this.history.length > 100) {
-      this.history.splice(0, 1);
-    }
-  }
+    this.show = function() {
 
-  this.show = function() {
-    stroke(0);
-    noFill();
-    ellipse(this.x, this.y, 24, 24);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
 
-    beginShape();
-    for (var i = 0; i < this.history.length; i++) {
-      var pos = this.history[i];
-      ellipse(pos.x, pos.y, i, i);
+        beginShape();
+        for (var i = 0; i < this.history.length; i++) {
+            var pos = this.history[i];
+            var r = random(0, 255);
+            var g = random(0, 255);
+            var b = random(0, 255);
+            stroke(r, g, b);
+            ellipse(pos.x, pos.y, i, i);
+        }
+        endShape();
     }
-    endShape();
-  }  
 }
